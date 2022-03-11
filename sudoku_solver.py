3c3580e844221b250c298c5d3ee867e6afe2a03f
Meryem Tuğba Nar <45947419+meryemnar@users.noreply.github.com>
2022-03-11 18:44:35 +0300

    Update sudoku_solver.py

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,25 +1,88 @@
 import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from PIL import Image
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+from skimage.transform import resize
 
+
+def splitcells(img):
+    rows = np.vsplit(img, 9)
+    boxes = []
+    for r in rows:
+        cols = np.hsplit(r, 9)
+        for box in cols:
+            boxes.append(box)
+    return boxes
+
+def CropCell(cells):
+    Cells_croped = []
+    for image in cells:
+        img = np.array(image)
+        img = img[4:46, 6:46]
+        img = Image.fromarray(img)
+        Cells_croped.append(img)
+    return Cells_croped
 
 def input_reader(picture_path):
-	"""
-	This function takes the path of input picture.
-	Then, it will return 2D matrix as python list.
-	Example input picture can be found in directory.
-	Example output:
-	matrix = [
-		[0,0,0,2,6,0,7,0,1],
-		[6,8,0,0,7,0,0,9,0],
-		[1,9,0,0,0,4,5,0,0],
-		[8,2,0,1,0,0,0,4,0],
-		[0,0,4,6,0,2,9,0,0],
-		[0,5,0,0,0,3,0,2,8],
-		[0,0,9,3,0,0,0,7,4],
-		[0,4,0,0,5,0,0,3,6],
-		[7,0,3,0,1,8,0,0,0]
-	]
-	"""
-	return matrix
+    #img = cv2.imread('mysudoku.jpg')
+    img = cv2.imread('sudoku.png')
+    sudoku_a = cv2.resize(img, (450,450))
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (3,3),6)
+    blur = cv2.bilateralFilter(gray,9,75,75)
+    threshold = cv2.adaptiveThreshold(blur,255,1,1,11,2)
+    plt.figure()
+    plt.imshow(threshold)
+    plt.show()
+
+    contour_1 = sudoku_a.copy()
+    contour_2 = sudoku_a.copy()
+    contour, hierarchy = cv2.findContours(threshold,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(contour_1, contour,-1,(0,255,0),3)
+    sudoku_cell = splitcells(threshold)
+
+    sudoku_cell_croped= CropCell(sudoku_cell)
+
+    model = tf.keras.models.load_model('model-OCR.h5')
+    #model = tf.keras.models.load_model('mnist.h5')
+    #model = tf.keras.models.load_model('mnistModel.h5')
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    predicted_numbers = []
+    length_of_sudoku = range(len(sudoku_cell_croped))
+
+    for i in length_of_sudoku:
+      foto = np.invert(np.array(sudoku_cell_croped[i]))
+      foto = resize(foto, (48, 48, 1))
+      prediction = model.predict(foto.reshape(1, 48, 48, 1))
+      index = np.argmax(prediction)
+      if index == 0:
+        predicted_numbers.append(0)
+      elif index == 1:
+        predicted_numbers.append(1)
+      elif index == 2:
+        predicted_numbers.append(2)
+      elif index == 3:
+        predicted_numbers.append(3)
+      elif index == 4:
+        predicted_numbers.append(4)
+      elif index == 5:
+        predicted_numbers.append(5)
+      elif index == 6:
+        predicted_numbers.append(6)
+      elif index == 7:
+        predicted_numbers.append(7)
+      elif index == 8:
+        predicted_numbers.append(8)
+      elif index == 9:
+        predicted_numbers.append(9)
+
+    matrix = np.array(predicted_numbers).astype('uint8').reshape(9, 9)
+    print(matrix)
+    return matrix
 
 
 def pretty_print_sudoku_table(matrix):
@@ -101,3 +164,4 @@ if __name__ == "__main__":
 	pretty_print_sudoku_table(matrix)
 	matrix_solved = solver(matrix)
 	print("Sudoku is solved!")
+
